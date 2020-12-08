07565db96f9de8088e3be620324f9cb3bfce8dcf
chenxianyue <1638057926@qq.com>
2020-12-08 14:59:39 +0800

    add

--- udpApp.py
+++ udpApp.py
@@ -16,11 +16,12 @@ class MainWindow(QMainWindow):
         # 初始化界面
         self.ui.setupUi(self)
         self.__actionBlinding__()
-        self.ui.udpIp.setText('192.168.1.3')
-        self.ui.udpPort.setText('5000')
+        self.udpIp = '192.168.1.3'
+        self.udpPort = '5000'
 
     def __actionBlinding__(self):
         self.ui.openFile.clicked.connect(self.fileOpenAck)
+        self.ui.actionUDP.triggered.connect(self.udpManuCreate)
 
     def fileOpenAck(self):
         dialog = QFileDialog()
@@ -39,12 +40,64 @@ class MainWindow(QMainWindow):
             self.fileMesg = filedHead.read()
             filedHead.close()
             self.udpSendFile()
+    def udpManuCreate(self):
+        dialog = QDialog()
+        dialog.setWindowTitle('UDP CONFIG')
+        dialog.setWindowModality(Qt.ApplicationModal)
+        dialog.setWindowIcon(QIcon('./ico/1.png'))
+        vtLayout = QVBoxLayout()
+        layout1 = QGridLayout()
+        groupBox1 = QGroupBox('Socket状态')
+        groupBox3 = QGroupBox('数据发送窗口(文本模式)')
 
+        groupBox1.setFont('黑体')
+        groupBox3.setFont('黑体')
+        label1 = QLabel('UDP IP')
+        label2 = QLabel('UDP 端口号')
+        textEditIP = QLineEdit()
+        textEditIP.setPlaceholderText('输入UDP IP')
+        textEditIP.setText(self.udpIp)
+        textEditPort = QLineEdit()
+        textEditPort.setPlaceholderText('输入UDP 端口号')
+        textEditPort.setText(self.udpPort)
+        layout1.addWidget(label1, 0, 0, 1, 1)
+        layout1.addWidget(textEditIP, 0, 1, 1, 2)
+        layout1.addWidget(label2, 0, 4, 1, 1)
+        layout1.addWidget(textEditPort, 0, 5, 1, 2)
+        groupBox1.setLayout(layout1)
+
+        self.ui.textEditTx = QTextEdit()
+        self.ui.pushButtonSocketSend = QPushButton('发送数据')
+        self.ui.pushButtonSocketCrcSend = QPushButton('发送清空')
+        self.ui.pushButtonSocketCrcRX = QPushButton('统计清空')
+        self.ui.pushButtonSocketSend.setFont('楷体')
+        self.ui.pushButtonSocketCrcSend.setFont('楷体')
+        self.ui.pushButtonSocketCrcRX.setFont('楷体')
+        layout2 = QGridLayout()
+        layout2.addWidget(self.ui.textEditTx, 0, 0, 5, 6)
+        layout2.addWidget(self.ui.pushButtonSocketSend, 0, 7, 1, 1)
+        layout2.addWidget(self.ui.pushButtonSocketCrcSend, 2, 7, 1, 1)
+        layout2.addWidget(self.ui.pushButtonSocketCrcRX, 4, 7, 1, 1)
+        groupBox3.setLayout(layout2)
+
+        vtLayout.addWidget(groupBox1)
+        vtLayout.addWidget(groupBox3)
+
+        vtLayout.setStretch(0, 1)
+        vtLayout.setStretch(1, 3)
+        dialog.setLayout(vtLayout)
+        textEditIP.editingFinished.connect(lambda :self.setUdpIp(textEditIP.text()))
+        textEditPort.editingFinished.connect(lambda: self.setUdpPort(textEditPort.text()))
+        if dialog.exec_():
+            pass
+    def setUdpIp(self, string):
+        self.udpIp = string
+    def setUdpPort(self, string):
+        self.udpPort = string
     def udpSendFile(self):
         ip_port = ('192.168.1.2', 6000)
         udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         udp_socket.bind(ip_port)
-        # print(self.fileMesg.encode())
         try:
             times = int(self.fileMesgSize / 1024)
             res = self.fileMesgSize % 1024
@@ -57,7 +110,7 @@ class MainWindow(QMainWindow):
                     sand = head + vp_addr[2:] + vp_len + \
                         self.fileMesg[i*1024:(i+1)*1024]
                     udp_socket.sendto(
-                        sand, (self.ui.udpIp.text(), int(self.ui.udpPort.text())))
+                        sand, (self.udpIp, int(self.udpPort)))
                     time.sleep(0.005)
             if (res != 0):
                 vp_addr = bytes(
@@ -66,8 +119,7 @@ class MainWindow(QMainWindow):
                     4, '0'), encoding='utf-8')
                 sand = head + vp_addr[2:] + vp_len + \
                     self.fileMesg[times * 1024: times * 1024 + res]
-                udp_socket.sendto(sand, (self.ui.udpIp.text(),
-                                         int(self.ui.udpPort.text())))
+                udp_socket.sendto(sand, (self.udpIp, int(self.udpPort)))
         except:
             self.tipErrorSocketSend()
         udp_socket.close()
--- window.py
+++ window.py
@@ -20,75 +20,20 @@ class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(744, 245)
+        MainWindow.resize(751, 105)
         self.actionUDP = QAction(MainWindow)
         self.actionUDP.setObjectName(u"actionUDP")
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
         self.verticalLayout_2 = QVBoxLayout(self.centralwidget)
         self.verticalLayout_2.setObjectName(u"verticalLayout_2")
-        self.groupBox = QGroupBox(self.centralwidget)
-        self.groupBox.setObjectName(u"groupBox")
-        self.verticalLayout = QVBoxLayout(self.groupBox)
-        self.verticalLayout.setObjectName(u"verticalLayout")
-        self.horizontalLayout_2 = QHBoxLayout()
-        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
-        self.label = QLabel(self.groupBox)
-        self.label.setObjectName(u"label")
-
-        self.horizontalLayout_2.addWidget(self.label)
-
-        self.horizontalSpacer = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
-
-        self.horizontalLayout_2.addItem(self.horizontalSpacer)
-
-        self.udpIp = QLineEdit(self.groupBox)
-        self.udpIp.setObjectName(u"udpIp")
-        self.udpIp.setMinimumSize(QSize(0, 40))
-
-        self.horizontalLayout_2.addWidget(self.udpIp)
-
-        self.horizontalLayout_2.setStretch(0, 1)
-        self.horizontalLayout_2.setStretch(1, 1)
-        self.horizontalLayout_2.setStretch(2, 2)
-
-        self.verticalLayout.addLayout(self.horizontalLayout_2)
-
-        self.horizontalLayout_3 = QHBoxLayout()
-        self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
-        self.label_2 = QLabel(self.groupBox)
-        self.label_2.setObjectName(u"label_2")
-
-        self.horizontalLayout_3.addWidget(self.label_2)
-
-        self.horizontalSpacer_2 = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
-
-        self.horizontalLayout_3.addItem(self.horizontalSpacer_2)
-
-        self.udpPort = QLineEdit(self.groupBox)
-        self.udpPort.setObjectName(u"udpPort")
-        self.udpPort.setMinimumSize(QSize(0, 40))
-
-        self.horizontalLayout_3.addWidget(self.udpPort)
-
-        self.horizontalLayout_3.setStretch(0, 1)
-        self.horizontalLayout_3.setStretch(1, 2)
-        self.horizontalLayout_3.setStretch(2, 1)
-
-        self.verticalLayout.addLayout(self.horizontalLayout_3)
-
-
-        self.verticalLayout_2.addWidget(self.groupBox)
-
-        self.verticalSpacer = QSpacerItem(20, 0, QSizePolicy.Minimum, QSizePolicy.Expanding)
-
-        self.verticalLayout_2.addItem(self.verticalSpacer)
-
         self.horizontalLayout = QHBoxLayout()
         self.horizontalLayout.setObjectName(u"horizontalLayout")
         self.fileDir = QLineEdit(self.centralwidget)
         self.fileDir.setObjectName(u"fileDir")
         self.fileDir.setMinimumSize(QSize(0, 40))
+        self.fileDir.setEchoMode(QLineEdit.Normal)
+        self.fileDir.setReadOnly(True)
 
         self.horizontalLayout.addWidget(self.fileDir)
 
@@ -108,18 +53,11 @@ class Ui_MainWindow(object):
 
         self.verticalLayout_2.addLayout(self.horizontalLayout)
 
-        self.verticalSpacer_2 = QSpacerItem(20, 0, QSizePolicy.Minimum, QSizePolicy.Expanding)
-
-        self.verticalLayout_2.addItem(self.verticalSpacer_2)
-
-        self.verticalLayout_2.setStretch(0, 2)
-        self.verticalLayout_2.setStretch(1, 1)
-        self.verticalLayout_2.setStretch(2, 1)
-        self.verticalLayout_2.setStretch(3, 1)
+        self.verticalLayout_2.setStretch(0, 1)
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(MainWindow)
         self.menubar.setObjectName(u"menubar")
-        self.menubar.setGeometry(QRect(0, 0, 744, 23))
+        self.menubar.setGeometry(QRect(0, 0, 751, 23))
         self.menu = QMenu(self.menubar)
         self.menu.setObjectName(u"menu")
         MainWindow.setMenuBar(self.menubar)
@@ -138,9 +76,6 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
         self.actionUDP.setText(QCoreApplication.translate("MainWindow", u"UDP", None))
-        self.groupBox.setTitle(QCoreApplication.translate("MainWindow", u"Socket\u8bbe\u7f6e", None))
-        self.label.setText(QCoreApplication.translate("MainWindow", u"UDP IP\u5730\u5740", None))
-        self.label_2.setText(QCoreApplication.translate("MainWindow", u"UDP \u7aef\u53e3\u53f7", None))
         self.openFile.setText(QCoreApplication.translate("MainWindow", u"\u6253\u5f00\u6587\u4ef6", None))
         self.menu.setTitle(QCoreApplication.translate("MainWindow", u"\u8bbe\u7f6e", None))
     # retranslateUi
